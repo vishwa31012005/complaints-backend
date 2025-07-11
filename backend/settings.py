e21a4aeda9c3ab84a845a79b68fe15f4d311a52c
Vishwa J <vishwaofficial2005@gmail.com>
2025-07-11 16:47:46 +0530

    Prepare for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(-#k=e#*02jzo2q)g4@6e3b^v6_8!-$(ya_tfd)ykyvr-)_(g)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
