2339ca232c24ccb6d5432c64a92b082275f25912
Vishwa J <vishwaofficial2005@gmail.com>
2025-07-11 17:24:11 +0530

    Fix: added missing 'import os' to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from pathlib import Path
 
 from pathlib import Path
 
